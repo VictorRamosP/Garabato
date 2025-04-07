eec8d2fef466546484efd4d5a433abcccc36b934
Vyktor <victureca3@gmail.com>
2025-04-07 19:59:28 +0200

    NormalEnemy

---
+++ Garabato/Assets/Scripts/Enemy/Patrol.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float patrolspeed;
+    private float disFloorDetection = 2;
+    private float disObstacleDetection = 2;
+
+    private bool infFloor;
+    private bool infObstacles;
+    private Rigidbody2D _rb;
+    public Transform contrfloor;
+    public Transform contrObst;
+    public LayerMask layerFloor;
+    public LayerMask layerobstacles;
+    private bool mirandoder = true;
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+    }
+    private void Update()
+    {
+        Patrolfunc();
+    }
+    public void Patrolfunc()
+    {
+        _rb.velocity = new Vector2(mirandoder ? patrolspeed : -patrolspeed, _rb.velocity.y);
+
+        infObstacles = Physics2D.Raycast(contrObst.position, transform.right, disObstacleDetection, layerobstacles);
+        infFloor = Physics2D.Raycast(contrfloor.position, Vector2.down, disFloorDetection, layerFloor);
+
+        if (infObstacles || !infFloor)
+        {
+            Girar(ref mirandoder);
+        }
+    }
+
+    private void Girar(ref bool mirandoder)
+    {
+        mirandoder = !mirandoder;
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+    }
+}
