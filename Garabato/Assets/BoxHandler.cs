09a4acc9764a28fb2cfce3bc3be92363ccbe69bc
izan302 <izan302.yt@gmail.com>
2025-05-07 15:14:13 +0200

    Final Polish

---
+++ Garabato/Assets/BoxHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D _rb;
+    private float gravityScale;
+    void Start()
+    {
+        gravityScale =  _rb.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ChangeCam.isMapActive) {
+            _rb.gravityScale = 0;
+        }else {
+            _rb.gravityScale = gravityScale;
+        }
+    }
+}
