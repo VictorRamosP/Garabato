8739be4359060ddfd218813e895ef5a82c97eb5f
Víctor Ramos <victureca3@gmail.com>
2025-05-07 18:37:46 +0200

    Arm implemented

--- Garabato/Assets/Scripts/Player/Shooting1/PlayerShoot.cs
+++ Garabato/Assets/Scripts/Player/Shooting1/PlayerShoot.cs
@@ -12,9 +12,12 @@ public class PlayerShoot : MonoBehaviour
     private float coolDownTimer = 0f;
     public float bulletTimeDestroy = 2f; 
     private GameObject shooting;
+    public PlayerMove _playermove;
+    //public GameObject player;
     void Start()
     {
         shooting = GameObject.FindGameObjectWithTag("Weapon");
+        //_playermove = _playermove.GetComponent<PlayerMove>();
     }
     void Update()
     {
@@ -35,38 +38,48 @@ public class PlayerShoot : MonoBehaviour
 
     void RotateShoot()
     {
+        
         bool Shot = false;
-        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow) && _playermove.mirandoDerecha == true)
         {
             shooting.transform.rotation = Quaternion.Euler(0, 0, -45);
             Shot = true;
         }
-        else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow) && !_playermove.mirandoDerecha)
         {
             shooting.transform.rotation = Quaternion.Euler(0, 0, 45); 
             Shot = true;
-        }else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
+        }else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow) && !_playermove.mirandoDerecha)
         {
             shooting.transform.rotation = Quaternion.Euler(0, 0, 135); 
             Shot = true;
-        }else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))
+        }else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow) && _playermove.mirandoDerecha == true)
         {
             shooting.transform.rotation = Quaternion.Euler(0, 0, -135); 
             Shot = true;
-        }else if (Input.GetKey(KeyCode.RightArrow)) {
+        }else if (Input.GetKey(KeyCode.RightArrow) && _playermove.mirandoDerecha == true) {
             shooting.transform.rotation = Quaternion.Euler(0, 0, -90);
+           /* if (!_playermove.mirandoDerecha)
+            {
+                shooting.transform.rotation = Quaternion.Euler(-180, 0, -90);
+            }*/
             Shot = true;
-        }else if (Input.GetKey(KeyCode.LeftArrow)) {
+        }else if (Input.GetKey(KeyCode.LeftArrow) && !_playermove.mirandoDerecha) {
             shooting.transform.rotation = Quaternion.Euler(0, 0, 90);
+           /* if (_playermove.mirandoDerecha)
+            {
+                player.transform.rotation = Quaternion.Euler(0,180,0);
+                shooting.transform.rotation = Quaternion.Euler(-180, 0, 90);
+            }*/
             Shot = true;
         }else if (Input.GetKey(KeyCode.UpArrow))
         {
             shooting.transform.rotation = Quaternion.Euler(0, 0, 0);
             Shot = true;
-        }else if (Input.GetKey(KeyCode.DownArrow)) {
+        }/*else if (Input.GetKey(KeyCode.DownArrow)) {
             shooting.transform.rotation = Quaternion.Euler(0, 0, 180);
             Shot = true;
-        }
+        }*/
 
         if (Shot) {
             coolDownTimer = coolDownShoot;
