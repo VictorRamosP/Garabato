355e33f59a6c4ae9047377c06f2802eacd3eb750
Jopaib8 <jopaib8@gmail.com>
2025-05-12 18:19:01 +0200

    Musica + Bugs LvL1/Menu

--- Garabato/Assets/Scripts/MainMenu/MainMenuController.cs
+++ Garabato/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -7,7 +7,7 @@ public class MainMenuController : MonoBehaviour
     public void PlayGame()
     {
         GameManager.Instance.mapAnimationActivated = false;
-        SceneManager.LoadScene("Level 1"); 
+        SceneManager.LoadScene("LvL-1"); 
     }
 
     public void LevelSelector()
