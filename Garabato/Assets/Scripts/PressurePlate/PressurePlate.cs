92a41f34d86624786f99fe54cba49f74ffc77433
Vyktor <victureca3@gmail.com>
2025-05-20 23:44:28 +0200

    Animation Pressure Plate

--- Garabato/Assets/Scripts/PressurePlate/PressurePlate.cs
+++ Garabato/Assets/Scripts/PressurePlate/PressurePlate.cs
@@ -5,9 +5,15 @@ public class PressurePlate : MonoBehaviour
     public Door door; 
     public bool keepOpen = true; // Para indicar si la puerta se queda abierta al pasar una vez o hay que estar encima de la placa para que se abra
 
+    private Animator _animatior;
     private bool isPlayerOnPlate = false;
-    private bool hasActivated = false; 
+    private bool hasActivated = false;
 
+
+    private void Start()
+    {
+        _animatior = GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Box"))
@@ -16,11 +22,14 @@ public class PressurePlate : MonoBehaviour
             {
                 door.Open();
                 hasActivated = true;
+                _animatior.SetBool("isPressed", true);
             }
             else if (!keepOpen && !isPlayerOnPlate)
             {
                 door.Open();
                 isPlayerOnPlate = true;
+                _animatior.SetBool("isPressed", true);
+
             }
         }
     }
