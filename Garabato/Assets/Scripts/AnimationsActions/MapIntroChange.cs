620a6d1339946ab354b5fcf19c640bd8a78041c2
Jopaib8 <jopaib8@gmail.com>
2025-06-01 23:08:34 +0200

    Camara con zoom In en Idle

--- Garabato/Assets/Scripts/AnimationsActions/MapIntroChange.cs
+++ Garabato/Assets/Scripts/AnimationsActions/MapIntroChange.cs
@@ -7,18 +7,22 @@ public class MapIntroChange : MonoBehaviour
 {
     private CinemachineVirtualCamera playerCam;
     private CinemachineVirtualCamera mapCam;
+    private CinemachineVirtualCamera gameplayCam;
     public AudioClip drawingClip;
     public bool activateAnimationmap;
     void Start()
     {
         GameObject playerCamObj = GameObject.FindGameObjectWithTag("PlayerCam");
         GameObject mapCamObj = GameObject.FindGameObjectWithTag("MapCam");
+        GameObject gameplayCamObj = GameObject.FindGameObjectWithTag("GameplayCamera");
         playerCam = playerCamObj.GetComponent<CinemachineVirtualCamera>();
         mapCam = mapCamObj.GetComponent<CinemachineVirtualCamera>();
+        gameplayCam = gameplayCamObj.GetComponent<CinemachineVirtualCamera>();
 
         // Verifica si la animación ya fue mostrada
         if (GameManager.Instance != null && GameManager.Instance.mapAnimationActivated)
         {
+            gameplayCam.Priority = 0;
             playerCam.Priority = 10;
             mapCam.Priority = 0;
           
@@ -27,6 +31,7 @@ public class MapIntroChange : MonoBehaviour
         else
         {
             playerCam.Priority = 0;
+            gameplayCam.Priority = 0;
             mapCam.Priority = 10;
 
             if (drawingClip != null)
@@ -39,6 +44,7 @@ public class MapIntroChange : MonoBehaviour
     public void OnMapAnimationEnd()
     {
         playerCam.Priority = 10;
+        gameplayCam.Priority = 11;
         mapCam.Priority = 0;
         gameObject.SetActive(false);
 
