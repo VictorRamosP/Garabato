6ac2dbdea037b4f5e99a4a71d60f09c780e545c8
Vyktor <victureca3@gmail.com>
2025-06-03 21:42:52 +0200

    CamaraArreglada

---
+++ Garabato/Assets/Scripts/CameraLookAhead.cs
@@ -0,0 +1,32 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAhead : MonoBehaviour
+{
+    public CinemachineVirtualCamera virtualCamera;
+    private GameObject player;
+    public float leftOffset;
+    public float rightOffset;
+    public float smoothSpeed = 2f;
+
+    private CinemachineFramingTransposer transposer;
+    private float targetScreenX;
+
+    void Start()
+    {
+       
+        player = GameObject.FindGameObjectWithTag("Player");
+        transposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        targetScreenX = transposer.m_ScreenX;
+    }
+
+    void Update()
+    {
+        bool lookingRight = player.transform.localScale.x > 0;
+        targetScreenX = lookingRight ? rightOffset : leftOffset;
+
+        transposer.m_ScreenX = Mathf.Lerp(transposer.m_ScreenX, targetScreenX, Time.deltaTime * smoothSpeed);
+    }
+}
